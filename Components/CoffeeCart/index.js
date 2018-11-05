695cc4c0619f5c971cc8a053cf96b3a84e0561f6
fawaz <q8fawazo@hotmail.com>
2018-11-05 07:52:20 +0300

    Task01

---
+++ Components/CoffeeCart/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+
+// NativeBase Components
+import {
+  Text,
+  Left,
+  Body,
+  Right,
+  List,
+  Button,
+  ListItem,
+  Icon
+} from "native-base";
+
+class CoffeeCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          drink: "Latte",
+          option: "Small",
+          quantity: 2
+        },
+        {
+          drink: "Espresso",
+          option: "Large",
+          quantity: 1
+        }
+      ]
+    };
+  }
+  renderItem(item, index) {
+    return (
+      <ListItem key={index}>
+        <Left>
+          <Text style={{ color: "white", marginLeft: 16 }}> {item.drink} </Text>
+          <Text note style={{ marginLeft: 16 }}>
+            {item.option}
+          </Text>
+        </Left>
+        <Body>
+          <Text style={{ color: "white" }}>{item.quantity}</Text>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Icon name="trash" style={{ color: "white", fontSize: 21 }} />
+          </Button>
+        </Right>
+      </ListItem>
+    );
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <List>
+        {list.map((item, index) => this.renderItem(item, index))}
+        <Button full danger>
+          <Text>Checkout</Text>
+        </Button>
+      </List>
+    );
+  }
+}
+
+export default CoffeeCart;
---
+++ Components/CoffeeList/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { ImageBackground, View, TouchableOpacity } from "react-native";
+
+// NativeBase Components
+import {
+  List,
+  ListItem,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Left,
+  Content
+} from "native-base";
+
+// Style
+import styles from "./styles";
+
+// List
+import coffeeshops from "./list";
+
+class CoffeeList extends Component {
+  handlePress() {
+    alert("Pressed");
+  }
+  renderItem(shop) {
+    return (
+      <TouchableOpacity key={shop.id} onPress={() => this.handlePress(shop)}>
+        <ImageBackground source={shop.background} style={styles.background}>
+          <View style={styles.overlay} />
+          <ListItem style={styles.transparent}>
+            <Card style={styles.transparent}>
+              <CardItem style={styles.transparent}>
+                <Left>
+                  <Thumbnail
+                    bordered
+                    source={shop.img}
+                    style={styles.thumbnail}
+                  />
+                  <Text style={styles.text}>{shop.name}</Text>
+                  <Text note style={styles.text}>
+                    {shop.distance}
+                  </Text>
+                </Left>
+              </CardItem>
+            </Card>
+          </ListItem>
+        </ImageBackground>
+      </TouchableOpacity>
+    );
+  }
+  render() {
+    let ListItems = coffeeshops.map(shop => this.renderItem(shop));
+    return (
+      <Content>
+        <List>{ListItems}</List>
+      </Content>
+    );
+  }
+}
+
+export default CoffeeList;
