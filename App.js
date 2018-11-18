a8c24fccc5cdda1e4dddf5b7d40422b71d9098e7
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2018-11-18 12:05:43 +0300

    Update App.js

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     Expo.Font.loadAsync({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
